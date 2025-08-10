8c4a150539f22e37c1f79288e65d6bc0e149bc02
THOKCHOM161 <lavaithanh@gmail.com>
2025-08-10 14:46:48 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+
+const CACHE_NAME = 'sohaib-mobile-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  
+  'https://cdn-icons-png.flaticon.com/512/2965/2965300.png',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'download.webp',
+  'happy.webp'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
